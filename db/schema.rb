fea3f63d69885d2cefa0995e2afdfd57d8201c41
Martin Mochetti <martin.mochetti@gmail.com>
2022-10-08 14:15:32 -0300

    change order of migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_08_145528) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_08_145529) do
   create_table "entities", force: :cascade do |t|
     t.integer "word_start_index", null: false
     t.integer "word_end_index", null: false
