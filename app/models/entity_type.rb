e0ac3e36179e585d4c10a173086bbf9631a9b33a
Martin Mochetti <martin.mochetti@gmail.com>
2022-10-11 07:56:37 -0300

    Add text color parsing

--- app/models/entity_type.rb
+++ app/models/entity_type.rb
@@ -1,4 +1,16 @@
 class EntityType < ApplicationRecord
   validates :text, presence: true, uniqueness: true
   validates :color, presence: true
+
+  def text_color
+    # Background color is stored in HEX format
+    hex_color = color.gsub('#','')
+    r, g, b = hex_color.scan(/../).map { |color| color.hex }
+
+    # HSP (Highly Sensitive Poo) equation from http://alienryderflex.com/hsp.html
+    hsp = Math.sqrt(0.299 * (r * r) + 0.587 * (g * g) + 0.114 * (b * b))
+
+    # Using the HSP value, determine whether the color is light or dark
+    hsp > 127.5 ? 'black' : 'white'
+  end
 end
--- spec/models/entity_type_spec.rb
+++ spec/models/entity_type_spec.rb
@@ -10,4 +10,16 @@ RSpec.describe EntityType, type: :model do
   it { should validate_presence_of(:text) }
   it { should validate_uniqueness_of(:text) }
   it { should validate_presence_of(:color) }
+
+  describe '#text_color' do
+    it 'returns white for dark colors' do
+      dark_entity_type = create(:entity_type, color: '#3e1313')
+      expect(dark_entity_type.text_color).to eq 'white'
+    end
+
+    it 'returns black for light colors' do
+      light_entity_type = create(:entity_type, color: '#7feceb')
+      expect(light_entity_type.text_color).to eq 'black'
+    end
+  end
 end
